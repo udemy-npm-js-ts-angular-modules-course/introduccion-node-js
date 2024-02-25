7306bf7108b58c7c919aa59c51407f377002180c
Gustavo Conturzo <gconturzo@gmail.com>
2024-02-25 10:38:40 -0300

    Módulo sencillo con saludos creado

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+    hola: function() {
+        console.log("Hola Mundo!!!");
+    },
+    holaPersonalizado: function(mombre) {
+        console.log(`Hola ${mombre}`);
+    },
+    holaAlCursoNPM: function() {
+        console.log("Hola al curso NPM");
+    }
+}
